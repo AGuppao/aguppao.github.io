b024e1b6ec263e6a0e19befe659f2e1fdb0e4028
AGuppao <135054743+AGuppao@users.noreply.github.com>
2024-07-24 13:31:18 +0100

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -1,23 +1,4 @@
-window.onscroll = function() {
-    var elems = document.getElementsByClassName('scroll-fade-in-out');   
-
-    for (var i = 0; i < elems.length; i++) {
-        var elem = elems[i];
-        var elemTop = elem.offsetTop - window.scrollY;
-        var elemHeight = elem.offsetHeight;
-        var padding = 20;
-
-        if (elemTop < window.innerHeight - elemHeight - padding && elemTop > padding) {
-            elem.classList.remove('hidden');
-            elem.classList.add('visible');
-        }
-        else
-        {
-            elem.classList.remove('visible');
-            elem.classList.add('hidden');
-        }
-    }
-
+function pausebgv() {
     var bgv = document.getElementById("bgv");
     var bgvRect = bgv.getBoundingClientRect();
     var bgvBottom = bgvRect.bottom;
@@ -33,3 +14,5 @@ window.onscroll = function() {
         }
     }
 };
+
+window.addEventListener('scroll', pausebgv)
