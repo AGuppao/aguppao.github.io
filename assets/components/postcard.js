d846911f97e0de0a2ef3456711125eadc645095a
AGuppao <135054743+AGuppao@users.noreply.github.com>
2024-08-17 19:58:43 +0100

    Add files via upload

--- assets/components/postcard.js
+++ assets/components/postcard.js
@@ -1,108 +1,111 @@
-class Postcard extends HTMLElement {
-    constructor() {
-        super();
-        
-        let subContainer = document.createElement('div');
-        subContainer.classList.add('sub-card');
+document.addEventListener('DOMContentLoaded', () => {
+    class Postcard extends HTMLElement {
+        constructor() {
+            super();
+            
+            let subContainer = document.createElement('div');
+            subContainer.classList.add('sub-card');
 
-        let titleElem = document.createElement('h2');
-        titleElem.classList.add('title');
-        titleElem.textContent = this.textContent;
-        this.textContent = "";
-        
-        let lowermeta = document.createElement('div');
-        lowermeta.classList.add('lower-meta');
+            let titleElem = document.createElement('h2');
+            titleElem.classList.add('title');
+            titleElem.textContent = this.textContent;
+            this.textContent = "";
+            
+            let lowermeta = document.createElement('div');
+            lowermeta.classList.add('lower-meta');
 
-        for(let kwarg of this.kwargs) {
-            let kwargElem = document.createElement('p');
-            kwargElem.classList.add('kwarg');
-            kwargElem.textContent = kwarg;
-            lowermeta.appendChild(kwargElem);
+            for(let kwarg of this.kwargs) {
+                let kwargElem = document.createElement('p');
+                kwargElem.classList.add('kwarg');
+                kwargElem.textContent = kwarg;
+                lowermeta.appendChild(kwargElem);
+            }
+
+            subContainer.appendChild(titleElem);
+            subContainer.appendChild(lowermeta);
+
+            let uppermeta = document.createElement('div');
+            uppermeta.classList.add('upper-meta');
+
+            let dateElem = document.createElement('p');
+            dateElem.classList.add('date');
+            dateElem.textContent = this.date;
+            
+            let readtimeElem = document.createElement('p');
+            readtimeElem.classList.add('read-time');
+            readtimeElem.textContent = this.readtime + " min";
+
+            uppermeta.appendChild(dateElem);
+            uppermeta.appendChild(readtimeElem);
+            
+            this.appendChild(uppermeta);
+            this.appendChild(subContainer);
         }
 
-        subContainer.appendChild(titleElem);
-        subContainer.appendChild(lowermeta);
+        get title() {
+            return this.getAttribute('title') || "Untitled";
+        }
 
-        let uppermeta = document.createElement('div');
-        uppermeta.classList.add('upper-meta');
+        set title(value) {
+            this.setAttribute('title', value);
+        }
 
-        let dateElem = document.createElement('p');
-        dateElem.classList.add('date');
-        dateElem.textContent = this.date;
+        get date() {
+            return this.getAttribute('date') || "Unknown";
+        }
+
+        set date(value) {
+            this.setAttribute('date', value);
+        }
+
+        get readtime() {
+            return this.getAttribute('readtime') || "0";
+        }
+
+        set readtime(value) {
+            this.setAttribute('readtime', value);
+        }
+
+        get url() {
+            return this.getAttribute('url') || "#";
+        }
+
+        set url(value) {
+            this.setAttribute('url', value);
+        }
+
+        get kwargs() {
+            let kwargsString = this.getAttribute('kwargs') || "";
+            return kwargsString.split(/,|;/).map(term => term.trim());
+        }
+
+        set kwargs(value) {
+            this.setAttribute('kwargs', value);
+        }
+
+        get bgi() {
+            return this.getAttribute('bgi') || "";
+        }
+
+        set bgi(value) {    
+            this.setAttribute('bgi', value);
+        }
+
+        connectedCallback() {
+            this.addEventListener('click', () => {
+                document.querySelectorAll('body > :not(header):not(footer):not(script)').forEach(element => {
+                    element.style.animation = 'slide-out 0.6s forwards';
+                });
         
-        let readtimeElem = document.createElement('p');
-        readtimeElem.classList.add('read-time');
-        readtimeElem.textContent = this.readtime + " min";
-
-        uppermeta.appendChild(dateElem);
-        uppermeta.appendChild(readtimeElem);
-        
-        this.appendChild(uppermeta);
-        this.appendChild(subContainer);
-    }
-
-    get title() {
-        return this.getAttribute('title') || "Untitled";
-    }
-
-    set title(value) {
-        this.setAttribute('title', value);
-    }
-
-    get date() {
-        return this.getAttribute('date') || "Unknown";
-    }
-
-    set date(value) {
-        this.setAttribute('date', value);
-    }
-
-    get readtime() {
-        return this.getAttribute('readtime') || "0";
-    }
-
-    set readtime(value) {
-        this.setAttribute('readtime', value);
-    }
-
-    get url() {
-        return this.getAttribute('url') || "#";
-    }
-
-    set url(value) {
-        this.setAttribute('url', value);
-    }
-
-    get kwargs() {
-        let kwargsString = this.getAttribute('kwargs') || "";
-        return kwargsString.split(/,|;/).map(term => term.trim());
-    }
-
-    set kwargs(value) {
-        this.setAttribute('kwargs', value);
-    }
-
-    get bgi() {
-        return this.getAttribute('bgi') || "";
-    }
-
-    set bgi(value) {    
-        this.setAttribute('bgi', value);
-    }
-
-    connectedCallback() {
-        this.addEventListener('click', () => {
-            document.querySelectorAll('body > :not(header):not(footer):not(script)').forEach(element => {
-                element.style.animation = 'slide-out 0.6s forwards';
+                setTimeout(() => {
+                    window.location.href = this.url;
+                }, 500);
             });
-    
-            setTimeout(() => {
-                window.location.href = this.url;
-            }, 500);
-        });
 
-        this.style.backgroundImage = "linear-gradient(rgb(0,0,0,0.5), rgba(0,0,0,0.5)), " + `url(${this.bgi})`;
+            this.style.backgroundImage = "linear-gradient(rgb(0,0,0,0.5), rgba(0,0,0,0.5)), " + `url(${this.bgi})`;
+        }
     }
-}
 
-customElements.define('post-card', Postcard);
+    customElements.define('post-card', Postcard);
+});
+
